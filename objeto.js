99e4e295df00c762189ac4a6dec4e569c8822f49
Larenzinhaa <analaurendourado@gmail.com>
2025-02-24 15:43:48 -0300

    inicio

---
+++ objeto.js
@@ -0,0 +1,13 @@
+// Criando um objeto de aluno
+const aluno= {
+    nome: "Lauren",
+    idade: 23,
+
+    estudar: () => console.log
+    (`${aluno.nome} está estudando. `),
+ };
+
+ console.log(aluno.nome);
+ aluno.estudar();
+
+za
